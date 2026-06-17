e0253a29fec2adcfef62a107c832702078c57ff3
Koteem <wasilek0009@gmail.com>
2026-06-17 16:31:36 +0200

    Add project files.

---
+++ the-game-of-life/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace the_game_of_life
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
